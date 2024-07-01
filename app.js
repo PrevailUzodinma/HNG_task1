b572f6f1029949231abbd5377a90a80207646a67
PrevailUzodinma <prevailuzodinma@gmail.com>
2024-07-01 12:21:06 +0100

    insatlled relevant packages, set up my server file

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; 
+
+
+
+
+
+app.listen(() =>{
+    console.log(`Server is running at port ${PORT}`)
+})
